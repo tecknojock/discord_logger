9246a39c4fe76ec587437c41fe3f2353ac9ad6de
Andrea Pascal <andrea@anodium.net>
2016-09-08 15:53:41 -0400

    Implement server metadata dumping

--- dumpbot/bot.py
+++ dumpbot/bot.py
@@ -8,28 +8,34 @@ class DumpBot(discord.Client):
         self.config = Config()
         self.conn = sqlite3.connect(self.config.db)
         self.cur = self.conn.cursor()
-        self.cur.execute('''
-                            CREATE TABLE IF NOT EXISTS servers (
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS servers (
                                 TEXT PRIMARY KEY server_id,
                                 TEXT name
-                            );
-                        ''')
+                            );''')
 
     def run(self):
         print('Logging in...')
         super().run(self.config.token, bot=self.config.bot)
 
-    async def on_message(self, message):
+    async def on_read(self, message):
         print('Getting server list...')
-        for s in discord.Client.servers:
-            await dump_server(s)
+        for server in discord.Client.servers:
+            await dump_server(server)
 
-    async def dump_server(self, id):
-        #TODO: HOW DO
-        #print('Dumping server id={}...'.format(discord.server))
-        pass
+    async def dump_server(self, server):
+        print('Dumping server id={}...'.format(server.id))
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS ? (
+                                TEXT PRIMARY KEY channel_id,
+                                TEXT name,
+                                TEXT description
+                        );''', server.id)
+        for channel in server.channels:
+            self.cur.execute('INSERT INTO ? VALUES (?, ?, ?);', \
+                             server.id, channel.id, channel.name, channel.topic)
+            await dump_channel(channel)
 
-    async def dump_channel(self, id):
+    async def dump_channel(self, channel):
+        print('Dumping channel id={}...'.format(channel.id))
         pass
 
 if __name__ == '__main__':
