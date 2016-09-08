3f424c2b1efa370aeffbc7fe932ce8eb6bc5b363
Andrea Pascal <andrea@anodium.net>
2016-09-08 15:34:02 -0400

    Actually start implementing

--- dumpbot/bot.py
+++ dumpbot/bot.py
@@ -7,11 +7,29 @@ class DumpBot(discord.Client):
         super().__init__()
         self.config = Config()
         self.conn = sqlite3.connect(self.config.db)
+        self.cur = self.conn.cursor()
+        self.cur.execute('''
+                            CREATE TABLE IF NOT EXISTS servers (
+                                TEXT PRIMARY KEY server_id,
+                                TEXT name
+                            );
+                        ''')
 
     def run(self):
+        print('Logging in...')
         super().run(self.config.token, bot=self.config.bot)
 
     async def on_message(self, message):
+        print('Getting server list...')
+        for s in discord.Client.servers:
+            await dump_server(s)
+
+    async def dump_server(self, id):
+        #TODO: HOW DO
+        #print('Dumping server id={}...'.format(discord.server))
+        pass
+
+    async def dump_channel(self, id):
         pass
 
 if __name__ == '__main__':
