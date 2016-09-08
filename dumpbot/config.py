65f8266a5afe6bc56a20296c6c97d10b83fe0646
Andrea Pascal <andrea@anodium.net>
2016-09-08 10:18:03 -0400

    Add config stuff

--- dumpbot/config.py
+++ dumpbot/config.py
@@ -1,4 +1,15 @@
 import json
+import sys
 
 class Config():
-    pass
+    def __init__():
+        _json = None
+        try:
+            with open('config.json') as f:
+                _json = json.load(f)
+        except FileNotFoundError as e:
+            print('config.json not found. Aborting.')
+            sys.exit(1)
+        self.bot = _json['bot']
+        self.db = _json['db']
+        self.token = _json['token']
