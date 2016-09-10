db07fff4d6d38c06a8be31da4aa9bcba140c4c87
Andrea Pascal <andrea@anodium.net>
2016-09-10 00:14:33 -0400

    Add MORE logging

--- dumpbot/bot.py
+++ dumpbot/bot.py
@@ -8,6 +8,7 @@ logging.basicConfig(level=logging.INFO)
 class DumpBot(discord.Client):
     def __init__(self):
         super().__init__()
+        self.logger = logging.getLogger()
         self.config = Config()
         self.conn = sqlite3.connect(self.config.db)
         self.cur = self.conn.cursor()
@@ -16,12 +17,12 @@ class DumpBot(discord.Client):
     def run(self):
         super().run(self.config.token, bot=self.config.bot)
 
-    async def on_read(self, message):
+    async def on_ready(self, message):
         for server in discord.Client.servers:
             await dump_server(server)
 
     async def dump_server(self, server):
-        print('Dumping server id={}...'.format(server.id))
+        self.logger.info('Dumping server id=%s', server.id)
         self.cur.execute(self.config.sql['mkserver'], server.id)
         for channel in server.channels:
             self.cur.execute(self.config.sql['inschannel'], \
@@ -29,7 +30,7 @@ class DumpBot(discord.Client):
             await dump_channel(channel)
 
     async def dump_channel(self, channel):
-        print('Dumping channel id={}...'.format(channel.id))
+        self.logger.info('Dumping channel id=%s', channel.id)
         pass
 
 if __name__ == '__main__':
--- dumpbot/config.py
+++ dumpbot/config.py
@@ -1,22 +1,27 @@
 import json
+import logging
 import os
 import sys
 
 class Config():
     def __init__(self):
+        _logger = logging.getLogger()
         _json = None
         try:
             with open('config.json') as f:
                 _json = json.load(f)
         except FileNotFoundError as e:
-            print('config.json not found. Aborting.')
+            _logger.critical('config.json not found. Aborting.')
             sys.exit(1)
+
+        _logger.info('Parsing config.json')
         self.bot = bool(_json['bot'])
         self.db = str(_json['db'])
         self.token = str(_json['token'])
 
         self.sql = {}
 
+        _logger.info('Parsing SQL')
         _sql_path = os.path.join(os.path.dirname(__file__), '_sql')
 
         with open(os.path.join(_sql_path, 'mkserverlist.sql')) as f:
