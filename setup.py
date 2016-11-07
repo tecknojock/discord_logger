a2e3aae6b33b668a32d2924d0313e6c33e755df7
Andrea Pascal <andrea@anodium.net>
2016-11-06 20:22:52 -0400

    Add classifiers

--- setup.py
+++ setup.py
@@ -13,15 +13,40 @@ DESCRIPTION = LONG_DESCRIPTION[1]
 REQUIRES = read('requirements.txt').splitlines()
 
 setup(
-    name='discord.export',
+    name='discord-data-export',
     version='0.0.0',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    keywords="discord data dump export message dms script",
+    keywords="discord data dump export message server channel dms script",
     author='Andrea Pascal',
     author_email='andrea@anodium.net',
     url='https://github.com/anodium/discord-data-export',
-    packages=['discord.export', 'discord.tests'],
+    packages=['discordexport', 'test'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Communications',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet',
+        'Topic :: Internet :: Log Analysis',
+        'Topic :: System',
+        'Topic :: System :: Archiving',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: System :: Logging',
+        'Topic :: Utilities'
+    ],
     requires=REQUIRES,
     license='MIT'
 )
