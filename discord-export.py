10d8404fd409b381b5a1117ba9d40f1f4380a524
Andrea Pascal <andrea@anodium.net>
2017-01-13 16:51:34 -0400

    Reorganize a teeny bit

--- discord-export.py
+++ discord-export.py
@@ -2,10 +2,14 @@ import argparse
 import discord
 
 client = discord.Client()
-args = None
 
 
 def main():
+    parse_args()
+    client.run(args.token, bot=(args.user if args.user is not None else True))
+
+
+def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('token',
@@ -15,11 +19,11 @@ def main():
                         help='path to the root of the log database')
 
     parser.add_argument('-u', '--user',
-                        help="token passed is a USER token, NOT a BOT token",
+                        help="token passed is a USER token, NOT a bot token",
                         action="store_false")
 
+    global args  # Yeah yeah, I used a global. Sue me, I don't care. P:
     args = parser.parse_args()
-    client.run(args.token, bot=(args.user if args.user is not None else True))
 
 
 @client.event
@@ -31,6 +35,7 @@ async def on_ready():
 async def on_message(message):
     print('[{}] {}: {}'
           .format(message.timestamp, message.author, message.content))
+
 
 if __name__ == "__main__":
     main()
