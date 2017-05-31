c75d94f31cb139e1a26ee20941cdcc400910bd0f
Andrea Pascal <andrea@anodium.net>
2017-05-31 16:47:07 -0400

    Organize directory structure

--- src/discord.logger/daemon.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-client = discord.Client()
-
-
-def _main(token):
-    if token is None:
-        raise EnvironmentError(
-            "DISCORD_TOKEN is undefined. Did you forget to set it?")
--- tests/test_daemon.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import discord.logger.daemon as daemon
-
-
-def test_noenv():
-    with pytest.raises(EnvironmentError):
-        daemon._main(None)
